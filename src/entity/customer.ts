adc0cce5dbd43ae7b10942f51ac4fc380de8cd54
David Ribeiro <david.ribeiro@solidaridadnetwork.org>
2024-04-23 01:43:05 -0300

    create order service and tests

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -5,6 +5,7 @@ export default class Customer {
     private _name: string = "";
     private _address!: Address;
     private _active: boolean = false;
+    private _rewardPoints: number = 0;
 
     constructor(id: string, name: string) {
         this._id = id;
@@ -24,6 +25,10 @@ export default class Customer {
 
     get Address(): Address {
         return this._address;
+    }
+
+    get rewardPoints(): number {
+        return this._rewardPoints;
     }
 
     set Address(address: Address) {
@@ -62,6 +67,10 @@ export default class Customer {
 
     deactivate() {
         this._active = false;
+    }
+
+    addRewardPoints(points: number) {
+        this._rewardPoints += points;
     }
 
 }
