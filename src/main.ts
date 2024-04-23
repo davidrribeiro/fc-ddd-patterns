6643296739a461ccb573e598d268916b428c4e75
David Ribeiro <david.ribeiro@solidaridadnetwork.org>
2024-04-23 02:15:50 -0300

    create repository structure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import Address from "./entity/address";
-import Customer from "./entity/customer";
+import Address from "./domain/entity/address";
+import Customer from "./domain/entity/customer";
 
 let customer = new Customer("123", "David Ribeiro");
 const address = new Address("Rua 1", "Cidade 1", "Estado 1", "12345-678", "País 1");
